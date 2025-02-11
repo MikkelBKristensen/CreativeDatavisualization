a4d5b9afd614ff40a39299fc0e3f6590b041d0fc
Mikkel Kristensen <mbek@itu.dk>
2025-02-11 12:42:45 +0100

    setup for exercise 1.3

---
+++ CodingBasics/Ex3-Objects/index.js
@@ -0,0 +1,8 @@
+var w = 800;
+var h = 800;
+
+var canvas = d3.select('#canvas')
+                .append('svg')
+                .attr('width', w)
+                .attr('height', h)
+                .style('background-color', 'lightgrey');
