52a6f2ca5dd1656edf9c2c4e46b3cad11369bb7c
Mads Dalgaard Andersen <madsd@itu.dk>
2025-03-18 14:30:02 +0100

    Did something

--- CodingBasics/Ex8/Ex8-DearData/index.js
+++ CodingBasics/Ex8/Ex8-DearData/index.js
@@ -3,54 +3,65 @@ d3.csv("data.csv").then((data) => {
     const width = 800;
     const margin = { top: 20, right: 30, bottom: 40, left: 40 };
 
+    const scaleConst = 5;
+
+    const colormap = {
+        "terrible": "#ae2c0e",
+        "meh": "#8d909b",
+        "ok": "#f0e51d",
+        "fine": "#ec8517",
+        "great": "#35bd1d",
+        "awesome": "#e96fb8"
+    }
+
+    // Define shape generation functions
+    const shapeMap = {
+        "research": d3.symbol().type(d3.symbolDiamond).size(d => calcSize(d.hours, d.solo) * 50),
+        "coding": d3.symbol().type(d3.symbolCircle).size(d => calcSize(d.hours, d.solo) * 50),
+        "writing": d3.symbol().type(d3.symbolSquare).size(d => calcSize(d.hours, d.solo) * 50),
+        "meeting": d3.symbol().type(d3.symbolWye).size(d => calcSize(d.hours, d.solo) * 50),
+        "testing": d3.symbol().type(d3.symbolTriangle).size(d => calcSize(d.hours, d.solo) * 50),
+        "prep": d3.symbol().type(d3.symbolTriangle).size(d => calcSize(d.hours, d.solo) * 50)
+    }
+
+    function calcSize(hours, solo) {
+        return solo ? (scaleConst * hours) / 2 : scaleConst * hours;
+    }
 
     const svg = d3.select("#canvas")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
+        .style("background-color", "#fff9e8")
         .append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
     // x-axis scale
-    const x = d3.scaleBand()
-        .domain(data.map((_, i) => i))
+    const x = d3.scaleLinear()
+        .domain([0, 10])
         .range([0, width])
-        .padding(0.001);
 
     // y-axis scale
     const y = d3.scaleLinear()
-        .domain([0, d3.max(data, d => windScale[d.wind])])
+        .domain([0, 10])
         .nice()
         .range([height, 0]);
 
-    // x-axis
-    svg.append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(x).tickFormat(i => data[i].timestamp))
-        .append("text")
-        .attr("x", width / 2)
-        .attr("y", margin.bottom - 10)
-        .attr("fill", "#000")
-        .attr("text-anchor", "middle")
-        .text("Timestamp");
-
-    // y-axis
-    svg.append("g")
-        .call(d3.axisLeft(y).ticks(6))
-        .append("text")
-        .attr("x", -margin.left)
-        .attr("y", -10)
-        .attr("fill", "#000")
-        .attr("text-anchor", "start")
-        .text("Wind Strength");
-
-    // x-axis positioning
-    svg.selectAll("rect")
+    // Create a group for each data point
+    const points = svg.selectAll("g")
         .data(data)
-        .join("rect")
-        .attr("x", (_, i) => x(i))
-        .attr("y", d => y(windScale[d.wind]))
-        .attr("width", x.bandwidth())
-        .attr("height", d => height - y(windScale[d.wind]))
-        .attr("fill", d => d.color);
+        .join("g")
+        .attr("transform", (d, i) => `translate(${x(i % 10)}, ${y(4- Math.floor(i / 10))})`);
+
+    // Add the appropriate shape to each group
+    points.each(function(d) {
+        const shape = d.type && shapeMap[d.type] ? 
+                      shapeMap[d.type] : 
+                      d3.symbol().type(d3.symbolSquare).size(calcSize(d.hours, d.solo) * 50);
+        
+        d3.select(this)
+            .append("path")
+            .attr("d", shape)
+            .attr("fill", colormap[d.howGood]);
+    });
 });
