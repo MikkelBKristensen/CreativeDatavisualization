b12fe97809334a9e88692da20346470e463e00e3
Mikkel Kristensen <mbek@itu.dk>
2025-05-17 17:20:04 +0200

    SvelteKit Project

---
+++ CreativeDataVizExam/src/lib/components/SectionCO2Viz.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from 'svelte';
+  import { createCO2EmissionsViz } from '$lib/utils/createCO2EmissionsViz';
+
+  let containerRef;
+
+  onMount(() => {
+    createCO2EmissionsViz(containerRef);
+  });
+</script>
+
+<section class="section">
+  <div bind:this={containerRef}></div>
+</section>
+
+<style>
+  .section {
+    min-height: 100vh;
+    background-color: #1f1f1f;
+    padding: 2rem;
+  }
+
+  #canvas {
+    margin: 0 auto;
+    max-width: 100%;
+  }
+</style>
---
+++ CreativeDataVizExam/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import SectionBarChart from "$lib/components/SectionBarChart.svelte";
+  import SectionLineChart from "$lib/components/SectionLineChart.svelte";
+  import SectionScatterPlot from "$lib/components/SectionScatterPlot.svelte";
+  import SectionCO2Viz from "$lib/components/SectionCO2Viz.svelte";
+</script>
+
+<main>
+  <SectionBarChart />
+  <SectionLineChart />
+  <SectionScatterPlot />
+  <SectionCO2Viz />
+</main>
+
+<style>
+  main {
+    scroll-snap-type: y mandatory;
+    overflow-y: scroll;
+  }
+</style>
