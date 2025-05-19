51f8cc60b0dc519aba8cc3d30a6ce311b3daff04
Mikkel Kristensen <mbek@itu.dk>
2025-05-19 15:06:36 +0200

    Some more trail and error

--- CreativeDataVizExam/src/routes/+page.svelte
+++ CreativeDataVizExam/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
   import SectionCO2Viz from "$lib/components/SectionCO2Viz.svelte";
+  import Co2EmissionPage from "$lib/components/CO2EmissionPage.svelte";
 </script>
 
 <main>
-  <SectionCO2Viz />
+  <!-- <SectionCO2Viz /> -->
+  <Co2EmissionPage />
 </main>
 
 <style>
