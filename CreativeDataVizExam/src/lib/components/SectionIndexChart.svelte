2b7aa12f672550dded4ac0eb84c403da042bb970
Mikkel Kristensen <mbek@itu.dk>
2025-05-20 11:06:44 +0200

    Create svelte component for the index chart

---
+++ CreativeDataVizExam/src/lib/components/SectionIndexChart.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { onMount } from "svelte";
+  import { renderIndexChart } from "$lib/utils/indexChart.js";
+
+  let chartContainer;
+
+  onMount(() => {
+    renderIndexChart(chartContainer, "/data/dataENG.csv");
+  });
+</script>
+
+<section>
+  <h2>Denmark’s CO₂ Emissions Over Time</h2>
+  <div bind:this={chartContainer}></div>
+</section>
