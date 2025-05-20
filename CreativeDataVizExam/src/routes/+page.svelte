963982d064187fa74097abeca0beb4fe637689d5
Mikkel Kristensen <mbek@itu.dk>
2025-05-20 11:06:53 +0200

    Add the Svelte component to the main page

--- CreativeDataVizExam/src/routes/+page.svelte
+++ CreativeDataVizExam/src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
   import SectionHero from "$lib/components/SectionHero.svelte";
   import SectionIntro from "$lib/components/SectionIntro.svelte";
+  import SectionIndexChart from "$lib/components/SectionIndexChart.svelte";
 </script>
 
 <main>
   <SectionHero />
   <SectionIntro />
+  <SectionIndexChart />
 </main>
 
 <style>
